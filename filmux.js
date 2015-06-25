19aba7ec9f350efea6e8e13b18ce93fb0da4bf0f
Paulius <p.zebelys@gmail.com>
2015-06-25 20:27:01 +0300

    bla
    filmux.org ad block skriptas

---
+++ filmux.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name        filmux.org
+// @namespace   filmux
+// @include     http://filmux.org/*
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
+// @require     https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @version     1
+// @grant       GM_addStyle 
+// ==/UserScript==
+
+
+$("noindex").remove();
+
+waitForKeyElements('#bb6a', blockas);
+
+function blockas() {
+  $('#bb6a').hide();
+  $("#bb6a").remove();
+}
+
+GM_addStyle(' #bb6a { display: none !important;  } ');
